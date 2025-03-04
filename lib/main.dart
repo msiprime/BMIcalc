72f0094bbd5d280d4e0a39b4a337bca751369f56
Sadman6m <shadman.6amtech@gmail.com>
2025-03-04 11:27:02 +0600

    <feat>: feature 7 and 8 merge by msi and sadman

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main(){
   print('feature 1 and 2 is added');
   print('feature 1 and 2 is added');
   print("feature 6");
-  log('feature 7 is added by msi');
+  log('feature 7 and 8 is added by msi and sadman');
   Get.put(Bmicontroller());
   runApp(BMICalculator());
 }
